4b34e486561453b71f683cfae2f34b692e648721
wangqiyangx <wangkaiyang0815@gmail.com>
2025-04-28 21:40:32 +0800

    Initial Commit

---
+++ FictionGate/Item.swift
@@ -0,0 +1,20 @@
+//
+// Item.swift.swift
+// FictionGate
+//
+// Copyright © 2025 wangqiyangX.
+// All Rights Reserved.
+//
+
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FictionGateTests/FictionGateTests.swift
@@ -0,0 +1,18 @@
+//
+// FictionGateTests.swift.swift
+// FictionGate
+//
+// Copyright © 2025 wangqiyangX.
+// All Rights Reserved.
+//
+
+
+import Testing
+
+struct FictionGateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
